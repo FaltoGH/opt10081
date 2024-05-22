2e30bc30feb47129016647416f95fd79fe9b276b
SoobumNa <sb051380us@gmail.com>
2024-05-23 03:30:46 +0900

    Add AxKHOpenAPILib

--- opt10081/Program.cs
+++ opt10081/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AxKHOpenAPILib;
 
 namespace opt10081
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            var a = new AxKHOpenAPI();
         }
     }
 }
